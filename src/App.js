4bca498bd4db577f9aade3299c7f57c716bd786b
nandOdiaz <fernando_dias99@hotmail.com>
2021-04-24 19:11:33 -0300

    finishing the chat feed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
+
 import './App.css'
 
 const App = () => {
     return (
-        <ChatEngine 
-            height="100vh"
-            projectID="583f9af5-7c5a-4738-a758-36a9cf2ba4a2"
-            userName="Nando Diaz"
-            userSecret="12345678"
-        />
+        <ChatEngine
+			height='100vh'
+			userName='nandodiaz'
+			userSecret='JW1Ct0VgQCo'
+			projectID='3e041b2f-5a22-4985-a6bb-9995a4b4932d'
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps } /> }
+		/>
     )
 }
 
