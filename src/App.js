7e141af14a1756c209018d2aa738f4a0eca13cb6
nandOdiaz <fernando_dias99@hotmail.com>
2021-04-24 17:21:53 -0300

    first part of the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { ChatEngine } from 'react-chat-engine'
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="583f9af5-7c5a-4738-a758-36a9cf2ba4a2"
+            userName="Nando Diaz"
+            userSecret="12345678"
+        />
+    )
+}
+
+export default App
