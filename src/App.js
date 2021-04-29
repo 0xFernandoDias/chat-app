7c736e17c53a5db64eacd4d43a26f1742a32e7bb
nandOdiaz <fernando_dias99@hotmail.com>
2021-04-29 15:10:56 -0300

    the chat application is finished!

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
 const App = () => {
+	if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
 			height='100vh'
-			userName='nandodiaz'
-			userSecret='JW1Ct0VgQCo'
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
 			projectID='3e041b2f-5a22-4985-a6bb-9995a4b4932d'
             renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps } /> }
 		/>
